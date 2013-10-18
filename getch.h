e48e8b8b676a24008c164c02767d4d19b7402a57
Joe Werle <joseph.werle@gmail.com>
2013-10-18 12:01:21 -0400

    added signal handling support to restore stdin state

--- getch.h
+++ getch.h
@@ -7,17 +7,38 @@
 #include <string.h>
 #include <termios.h>
 #include <unistd.h>
+#include <signal.h>
 
 int
 getch ();
 
+static void
+on_signal (int signal);
+
+static struct termios _term;
+
+static int signals[] = {
+  SIGABRT,
+  SIGINT,
+  SIGKILL,
+  SIGQUIT,
+  SIGSTOP,
+  SIGTERM
+};
+
 int
 getch () {
   int c = EOF;
+  int i = 0;
+  int len = sizeof(signals) / sizeof(signals[0]);
   struct termios term, ttmp;
 
   // get stdin attributes
   tcgetattr(STDIN_FILENO, &term);
+
+  // get stdin attributes and store in static `_term variable for
+  // a global copy
+  tcgetattr(STDIN_FILENO, &_term);
 
   // store state of stdin
   ttmp = term;
@@ -28,6 +49,16 @@ getch () {
   // set stdin attributes on tmp termio struct now with `TCSANOW`
   tcsetattr(STDIN_FILENO, TCSANOW, &ttmp);
 
+  // bind signal handles
+  for (; i < len; ++i) {
+    signal(signals[i], on_signal);
+    signal(signals[i], on_signal);
+    signal(signals[i], on_signal);
+    signal(signals[i], on_signal);
+    signal(signals[i], on_signal);
+    signal(signals[i], on_signal);
+  }
+
   // get char from modified stdin
   c = getchar();
 
@@ -36,5 +67,13 @@ getch () {
 
   return c;
 }
+
+
+static void
+on_signal (int signal) {
+  tcsetattr(STDIN_FILENO, TCSANOW, &_term);
+  exit(1);
+}
+
 
 #endif
